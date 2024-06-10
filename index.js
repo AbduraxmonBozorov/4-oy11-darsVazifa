8be8212d49e7ba39130ceee97a3b004f0874c6e6
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-10 08:14:47 +0500

    4-oy 11-dars vazifasi yuklandi

---
+++ index.js
@@ -0,0 +1,54 @@
+// 1-masala
+// function getLastChar (str){
+//     return str[str.length-1];
+// }
+// console.log(getLastChar("Salom"));
+
+// 2-masala
+// function getMiddleChar(str) {
+//   let a = 0;
+//   if (str.length % 2 == 0) {
+//     a = str[str.length / 2 - 1] + str[str.length / 2];
+//   } else {
+//     let b = Math.floor(str.length / 2);
+//     a = str[b];
+//   }
+//   return a;
+// }
+// console.log(getMiddleChar("saloma"));
+
+// 3-masala
+// function getFirstCharCode(str){
+//     return str.charCodeAt(0);
+// }
+// console.log(getFirstCharCode("Salom"));
+
+// 4-masala
+// function getStringLengthDescription(str) {
+//   let text = new String();
+//   if (str.length > 10) {
+//     text = "Uzoq string";
+//   } else {
+//     text = "Qisqa string";
+//   }
+//   return str.length, text;
+// }
+// console.log(getStringLengthDescription("Salom"));
+
+// 5-masala
+// function getCharCodes(str){
+//     for(let i=0; i<str.length; i++){
+//         console.log(str.charCodeAt(i));
+//     }
+// }
+// getCharCodes("salom")
+
+
+
+
+// Stringlarda `slice`, `substring`, va `substr` metodlariga oid 7 ta masala funksiyalar bilan ishlaydigan qilib tuzilgan shartlari:
+
+// 1-masala
+function getFirstThreeChars(str){
+    
+}
